c7978096cb117ce59e036faf6b8e9767656a3e26
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2024-02-21 11:07:21 -0800

    Update 2

--- Functions.hpp
+++ Functions.hpp
@@ -4,7 +4,7 @@ class CfgFunctions
 	{
 		class functions
 		{
-		file="\l6AA_medkits\functions";
+		file="\medkits\functions";
 			class open_Surgery {};
 			class open_Transfusion {};
 			class open_DRUGS {};
